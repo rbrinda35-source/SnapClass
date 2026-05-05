5b706e3965d54477c6218594256738b2ccc1143b
Brinda Rathod <rbrinda35@gmail.com>
2026-05-05 22:22:40 +0530

    domain correction

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -5,7 +5,7 @@ import io
 
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
-    app_domain = "snapclass-main.streamlit.app"
+    app_domain = "snapclass-main0.streamlit.app"
     join_link = f"{app_domain}?join-code={subject_code}"
     
     st.header(f"Scan to Join")
